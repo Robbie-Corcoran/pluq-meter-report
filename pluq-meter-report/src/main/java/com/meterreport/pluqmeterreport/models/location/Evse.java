85504dbee6b20671c061ae145006520524b886bf
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-27 21:54:06 +0100

    Updated models to map JSON correctly

--- pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/models/location/Evse.java
+++ pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/models/location/Evse.java
@@ -3,6 +3,7 @@ package com.meterreport.pluqmeterreport.models.location;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import java.util.List;
 
@@ -10,11 +11,15 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Evse {
+
     private String uid;
+    @BsonProperty(value = "evse_id")
     private String evseId;
     private String status;
+    @BsonProperty(value = "last_updated")
     private String lastUpdated;
     private List<String> capabilities;
+    @BsonProperty(value = "physical_reference")
     private String physicalReference;
     private List<Connector> connectors;
 
--- pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/models/location/Location.java
+++ pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/models/location/Location.java
@@ -3,6 +3,7 @@ package com.meterreport.pluqmeterreport.models.location;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.bson.codecs.pojo.annotations.BsonProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -20,9 +21,13 @@ public class Location {
     private String type;
     private String address;
     private String city;
+    @BsonProperty(value = "postal_code")
     private String postalCode;
     private String country;
     private Coordinates coordinates;
+    @BsonProperty(value = "charging_when_closed")
+    private boolean chargingWhenClosed;
+    @BsonProperty(value = "last_updated")
     private String lastUpdated;
     private List<Evse> evses;
 
