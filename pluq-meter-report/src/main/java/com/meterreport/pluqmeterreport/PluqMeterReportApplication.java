c4dd0f5b0ddcebcc74c94df89e52e4f33518bcbe
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-27 15:46:22 +0100

    Project init

---
+++ pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/PluqMeterReportApplication.java
@@ -0,0 +1,13 @@
+package com.meterreport.pluqmeterreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluqMeterReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluqMeterReportApplication.class, args);
+	}
+
+}
---
+++ pluq-meter-report/src/test/java/com/meterreport/pluqmeterreport/PluqMeterReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meterreport.pluqmeterreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluqMeterReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
