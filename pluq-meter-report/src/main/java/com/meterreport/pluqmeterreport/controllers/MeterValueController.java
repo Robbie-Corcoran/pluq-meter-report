bd0fbcaa6e07cb9588aa5423b8ad206253820fdb
Robbie Corcoran <codewithrobbie@gmail.com>
2024-03-28 10:55:09 +0100

    MeterValueController get mapping setup

---
+++ pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/controllers/MeterValueController.java
@@ -0,0 +1,33 @@
+package com.meterreport.pluqmeterreport.controllers;
+
+import com.meterreport.pluqmeterreport.models.MeterValue;
+import com.meterreport.pluqmeterreport.services.MeterValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/meter-values")
+public class MeterValueController {
+
+    private final MeterValueService meterValueService;
+
+    @Autowired
+    public MeterValueController(MeterValueService meterValueService) {
+        this.meterValueService = meterValueService;
+    }
+
+    @GetMapping
+    public Optional<MeterValue> getMeterValueById(String meterValueId) {
+        return meterValueService.getMeterValueById(meterValueId);
+    }
+
+    @GetMapping("/all")
+    public List<MeterValue> getAllMeterValues() {
+        return meterValueService.getAllMeterValues(meterValueService);
+    }
+}
--- pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/services/MeterValueService.java
+++ pluq-meter-report/src/main/java/com/meterreport/pluqmeterreport/services/MeterValueService.java
@@ -6,6 +6,9 @@ import com.meterreport.pluqmeterreport.repos.MeterValueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MeterValueService {
 
@@ -19,4 +22,14 @@ public class MeterValueService {
     public MeterValue saveMeterValue(MeterValue meterValue) {
         return meterValueRepository.save(meterValue);
     }
+
+    public Optional<MeterValue> getMeterValueById(String meterValueId) {
+        return meterValueRepository.findById(meterValueId);
+    }
+
+    public List<MeterValue> getAllMeterValues(MeterValueService meterValueService) {
+        return meterValueRepository.findAll();
+    }
+
+
 }
